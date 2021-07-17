69feb9283f175cdbf2388e895eba2884befb5284
Wonseok Shin <wsshin97@gmail.com>
2021-07-17 00:53:31 -0400

    Add unit tests showing some example usages

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,10 @@ using SimpleConstants
 using Test
 
 @testset "SimpleConstants.jl" begin
-    # Write your tests here.
+    λ = rand()  # wavelength
+    @test 2πc₀/λ == 2π * c₀ / λ
+    @test ℎc₀/λ == ℎ * c₀ / λ
+
+    @test exp(i2π/3) == exp(im * 2π / 3)
+    @test exp(iπ/3) == exp(im * π / 3)
 end
