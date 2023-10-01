684365257dd98f7f0d7916829bc7c0f32635240c
Wonseok Shin <wsshin97@gmail.com>
2023-10-01 01:12:38 -0400

    Add unit tests for SIPrefix

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,4 +20,21 @@ using Test
     @test eV2meter(E_ineV) ≈ λ_inµm * micro
     @test meter2eV(λ_inµm * micro) ≈ E_ineV
     @test round(λ_inµm * E_ineV, digits=2) == 1.24  # (λ in µm) times (E in eV) is roughly 1.24
+
+    @test issorted(getfield.(SI_PREFIXES, :factor))
+    for prefix = SI_PREFIXES
+        x = π
+        @test x * prefix == x * prefix.factor
+        @test prefix * x == prefix.factor * x
+        @test x / prefix == x / prefix.factor
+        @test prefix / x == prefix.factor / x
+
+        xs = 1:10
+        @test x .* prefix == x .* prefix.factor
+        @test prefix .* x == prefix.factor .* x
+        @test x ./ prefix == x ./ prefix.factor
+        @test prefix ./ x == prefix.factor ./ x
+
+        @test "$prefix" == prefix.symbol
+    end
 end
